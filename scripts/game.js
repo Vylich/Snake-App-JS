6a7b0b01493a1974313ced66c8b3c91c21fc5f97
Vylich <ilya.sidorenko.99@gmail.com>
2023-02-28 16:24:09 +0300

    VERSION 2

---
+++ scripts/game.js
@@ -0,0 +1,34 @@
+import Canvas from "./components/canvas.js";
+import GameLoop from "./components/gameLoop.js";
+import Snake from "./components/snake.js";
+import Score from "./components/score.js";
+import Berry from "./components/berry.js";
+
+class Game {
+
+    constructor( container ) {
+
+        this.canvas = new Canvas( container );
+        this.snake = new Snake();
+        this.berry = new Berry( this.canvas );
+        this.score = new Score( ".game-score .score-count", 0 );
+        new GameLoop( this.update.bind(this), this.draw.bind(this) );
+
+    }
+
+    update() {
+        this.snake.update( this.berry, this.score, this.canvas );
+    }
+
+    draw() {
+
+        this.canvas.context.clearRect( 0, 0, this.canvas.element.width, this.canvas.element.height );
+
+        this.snake.draw( this.canvas.context );
+        this.berry.draw( this.canvas.context );
+
+    }
+
+}
+
+new Game( document.querySelector(".canvas-wrapper") );
